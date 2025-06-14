6769bc91897df125cd1740fa3175b62c63206a5b
duyanhsdte <duyanhsdte@gmail.com>
2025-06-14 17:41:42 +0700

    DuyAnh Test

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Chance <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          111 Learn React
         </a>
       </header>
     </div>
